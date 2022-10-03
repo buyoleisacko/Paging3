972ddff60a6334b04b0c349e9b7fd64f2313c3a3
Buyole Isacko <isackobuyole@gmail.com>
2022-10-03 17:54:08 +0300

    set up headers for photos and search images

--- app/src/main/java/com/example/pagin3demo/model/data/remote/UnsplashApi.kt
+++ app/src/main/java/com/example/pagin3demo/model/data/remote/UnsplashApi.kt
@@ -1,4 +1,23 @@
 package com.example.pagin3demo.model.data.remote
 
+import com.example.pagin3demo.BuildConfig
+import com.example.pagin3demo.model.UnsplashImage
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.Query
+
 interface UnsplashApi {
+    @Headers("Authorization: Client-ID ${BuildConfig.API_KEY}")
+    @GET("/photos")
+    suspend fun getAllImages(
+        @Query("page") page: Int,
+       @Query("per_page") per_page: Int
+    ): List<UnsplashImage>
+
+    @Headers("Authorization: Client-ID ${BuildConfig.API_KEY}")
+    @GET("/search/photos")
+    suspend fun searchImages(
+        @Query("page") page: Int,
+        @Query("per_page") per_page: Int
+    ): List<UnsplashImage>
 }
