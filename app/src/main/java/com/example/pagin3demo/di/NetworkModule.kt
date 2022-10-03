3b135c2f6309a502274430c6198964000733d2f6
Buyole Isacko <isackobuyole@gmail.com>
2022-10-03 18:42:21 +0300

    implemented the network module

--- app/src/main/java/com/example/pagin3demo/di/NetworkModule.kt
+++ app/src/main/java/com/example/pagin3demo/di/NetworkModule.kt
@@ -1,4 +1,50 @@
 package com.example.pagin3demo.di
 
+import com.example.pagin3demo.model.data.remote.UnsplashApi
+import com.example.pagin3demo.ui.util.Constants.BASE_URL
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
 object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .readTimeout(15, TimeUnit.SECONDS)
+            .connectTimeout(15, TimeUnit.SECONDS)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        val contentType = MediaType.get("application/json")
+        val json = Json {
+            ignoreUnknownKeys = true
+        }
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(Json.asConverterFactory(contentType))
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideUnsplashedApi(retrofit: Retrofit): UnsplashApi {
+        return retrofit.create(UnsplashApi::class.java)
+
+    }
 }
--- app/src/main/java/com/example/pagin3demo/ui/util/Constants.kt
+++ app/src/main/java/com/example/pagin3demo/ui/util/Constants.kt
@@ -2,5 +2,7 @@ package com.example.pagin3demo.ui.util
 
 object Constants {
 
+    const val BASE_URL = "https://api.unsplash.com"
+
     const val UNSPLASH_IMAGE_TABLE = "unsplash_image_table"
 }
