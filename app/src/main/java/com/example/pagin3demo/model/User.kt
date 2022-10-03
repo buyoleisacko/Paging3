77fa55ad2dc55ba7811653b400dad75ce92a7f77
Buyole Isacko <isackobuyole@gmail.com>
2022-10-03 17:08:45 +0300

    created user links data class

--- app/src/main/java/com/example/pagin3demo/model/User.kt
+++ app/src/main/java/com/example/pagin3demo/model/User.kt
@@ -1,4 +1,14 @@
 package com.example.pagin3demo.model
 
-class User {
-}
+import com.google.gson.annotations.SerializedName
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class User(
+    @SerialName("links")
+    val userLinks: UserLinks,
+    val userName: String
+
+
+)
---
+++ app/src/main/java/com/example/pagin3demo/model/UserLinks.kt
@@ -0,0 +1,5 @@
+package com.example.pagin3demo.model
+
+data class UserLinks(
+
+)
